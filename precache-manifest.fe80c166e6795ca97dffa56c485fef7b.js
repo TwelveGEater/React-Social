46ccaf71b50479e5b813b82db54355a0996deb81
dronkfc@gmail.com <dronkfc@gmail.com>
2020-08-31 18:00:15 +0300

    Updates

---
+++ precache-manifest.fe80c166e6795ca97dffa56c485fef7b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79c99b0de786f87e209db33ce3d355b",
+    "url": "/React-Social/index.html"
+  },
+  {
+    "revision": "7a70ae2c800bdb35624a",
+    "url": "/React-Social/static/css/main.f127bd8e.chunk.css"
+  },
+  {
+    "revision": "2ca31aa489ad8b0fd245",
+    "url": "/React-Social/static/js/2.ce5fd4ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Social/static/js/2.ce5fd4ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b363693cc2f7b4eb9465",
+    "url": "/React-Social/static/js/3.3601e51b.chunk.js"
+  },
+  {
+    "revision": "7a70ae2c800bdb35624a",
+    "url": "/React-Social/static/js/main.6a092df1.chunk.js"
+  },
+  {
+    "revision": "82de6302a34c32626c95",
+    "url": "/React-Social/static/js/runtime-main.f3b0bbba.js"
+  },
+  {
+    "revision": "181849d985d2519d0502f8624cce53e9",
+    "url": "/React-Social/static/media/loader.181849d9.gif"
+  },
+  {
+    "revision": "225a765e742db0bd75bab7ecde8a2c66",
+    "url": "/React-Social/static/media/user.225a765e.png"
+  }
+]);
